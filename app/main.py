fdeb7077da2b3b93167de73f77cff9f5f2877c28
dima <giomozda74@gmail.com>
2025-05-07 20:01:43 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+
+import requests
+from dotenv import load_dotenv
+from fastapi import FastAPI
+app = FastAPI()
+load_dotenv()
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
 
 
-if __name__ == "__main__":
-    get_weather()
+@app.get("/weather")
+def get_weather() -> any:
+    response = requests.get(URL + f"key={API_KEY}&q={CITY}")
+    data = response.json()
+    location_name = data["location"]["name"]
+    temperature = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    return {
+        "city": location_name,
+        "temperature": temperature,
+        "condition": condition
+    }
